3b35ff5346960f6093915bcf070ee3008b001eeb
Sabbir Hassan <sbbir0087@gmai.com>
2025-05-13 23:49:42 +0600

    Changed favicon color

--- src/builtins/cors.rs
+++ src/builtins/cors.rs
@@ -25,7 +25,8 @@ pub fn get_policy() -> Cors {
         // You can also add more constraints if needed
         Cors::default()
             // .allowed_origin("https://example.com")
-            .allowed_origin("https://bitlaab.com:446")
+            .allowed_origin("https://sabbirhassan.com")
+            .allowed_origin("https://www.sabbirhassan.com")
             .allow_any_header()
             .allow_any_method()
             .max_age(3600)
