a5ced126ba2a8b3ac9025be6c421c2fc5e1bacc8
Sabbir Hassan <sbbir0087@gmail.com>
2025-11-04 15:07:38 +0600

    Chnaged navigator

--- assets/js/home.js
+++ assets/js/home.js
@@ -3,11 +3,26 @@ function changeNavbar(event) {
     let wrapper = item.parentElement;
 
     let preSelected = wrapper.querySelector('.item.selected');
-    preSelected.style.width = `${20+36}px`;
+    preSelected.style.width = `${20 + 36}px`;
     preSelected.classList.remove('selected');
 
     item.classList.add('selected');
     item.style.width = `${item.getAttribute('full-width')}px`;
+
+    let preItemName = preSelected.getAttribute('item-name');
+    let itemName = item.getAttribute('item-name');
+
+    let preContent = document.querySelector(`#${preItemName}`);
+    let content = document.querySelector(`#${itemName}`);
+
+    if (content === null) return;
+
+    preContent.style.opacity = 0;
+    content.style.display = 'flex';
+    setTimeout(() => {
+        preContent.style.display = 'none';
+        content.style.opacity = 1;
+    }, 272);
 }
 
 window.addEventListener("load", () => {
@@ -15,15 +30,13 @@ window.addEventListener("load", () => {
     let navItems = navBar.querySelectorAll('.item');
 
     for (let item of navItems) {
-        console.log(item.clientWidth);
-        
         if (item.classList.contains('selected')) {
             item.setAttribute('full-width', item.clientWidth + 18);
             item.style.width = `${item.clientWidth + 18}px`;
         }
         else {
             item.setAttribute('full-width', item.clientWidth);
-            item.style.width = `${20 +36}px`; // 20px icon + 18px padding
+            item.style.width = `${20 + 36}px`; // 20px icon + 18px padding
         }
     }
 
